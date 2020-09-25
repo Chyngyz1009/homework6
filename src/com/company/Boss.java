8a952c5aa6c7d85ed0d351c666d80086fe4c421b
Chyngyz1009 <chika_0410@inbox.ru>
2020-09-25 22:02:45 +0600

    обновление д.з

--- src/com/company/Boss.java
+++ src/com/company/Boss.java
@@ -2,14 +2,13 @@ package com.company;
 
 public class Boss extends GameEntiti {
 
-    private Weapon weapon1=new Weapon();
+    private Weapon weapon1;
 
-    public String getWeapon1() {
-        return weapon1.getAvtomatic() + weapon1.getKalashnik();
+    public Weapon getWeapon1() {
+        return weapon1;
     }
 
-    public void setWeapon1(String avtomaticKalashnikov, String gun) {
-        weapon1.setAvtomatic(avtomaticKalashnikov);
-        weapon1.setKalashnik(gun);
+    public void setWeapon1(Weapon weapon1) {
+        this.weapon1 = weapon1;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         Boss boss1 = new Boss();
+        Weapon weapon=new Weapon();
+        weapon.setKalashnik("gygkhgkj");
+        weapon.setAvtomatic("ggu");
         boss1.setHealth(100);
         boss1.setAtack(120);
-        boss1.setWeapon1("avtomaticKalashnikova", "gun");
+        boss1.setWeapon1(weapon);
 
-        System.out.println(boss1.getHealth()+ " "+ boss1.getAtack()+  "  "+boss1.getWeapon1());
+
+        System.out.println(boss1.getHealth()+ " "+ boss1.getAtack()+  "  "+weapon.getKalashnik()+" "+weapon.getAvtomatic());
 
 
     }
